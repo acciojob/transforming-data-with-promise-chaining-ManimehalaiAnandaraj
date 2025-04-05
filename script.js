b87e1b83e935de522d40a6501fd11205bed2762d
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-04-05 12:18:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+function transformData(inputNumber) {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(inputNumber);
+	},2000);
+	})	
+	.then((number) => {
+		document.getElementById('output').textContent=`Result:${number}`;
+		return new Promise(resolve => setTimeout(()=> resolve(number * 2),2000));
+	})
+	.then((number) => {
+		document.getElementById('output').textContent=`Result:${number}`;
+		return new Promise(resolve => setTimeout(()=> resolve(number - 2),2000));
+	})
+	 .then((number) => {
+		document.getElementById('output').textContent=`Result:${number}`;
+		return new Promise(resolve => setTimeout(()=> resolve(number / 2),2000));
+	})
+	 .then((number) => {
+		document.getElementById('output').textContent=`Result:${number}`;
+		return new Promise(resolve => setTimeout(()=> resolve(number + 2),2000));
+	})
+	.then((number) => {
+		document.getElementById('output').textContent=`Final Result:${number}`;
+	});
+}
+
+
